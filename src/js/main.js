99ff32eb3296725363101c5aa53c4b45ea41a72a
Murad Karimov <muradkarimov07@gmail.com>
2024-01-23 01:21:36 +0300

    create download.js file

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import MiniSlider from "./modules/slider/slider-mini";
 import Difference from "./modules/difference";
 import Form from "./modules/forms";
 import ShowInfo from "./modules/showInfo";
+import Download from "./modules/download";
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns: '.next', container: '.page'});
@@ -47,4 +48,5 @@ window.addEventListener('DOMContentLoaded', () => {
 
     new ShowInfo('.plus__content').init();
 
+    new Download('.download').init();
 });
